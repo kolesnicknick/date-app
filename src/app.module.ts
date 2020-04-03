f3fcdea974035c9c7975f45091ad1683c76e8d8d
kolesnicknick <kolesniknikolai92@gmail.com>
2020-04-03 18:58:25 +0300

    update with neccessary stuff for model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const environment = process.env.NODE_ENV || 'development';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    AuthModule,
+
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(
+      process.env.MONGODB_WRITE_CONNECTION_STRING,
+      {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+      }
+    )
+  ],
 })
 export class AppModule {}
---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+import { genderEnum } from '../enums/gender.enum';
+
+export const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  avatar: { type: String, default: null},
+  avatarId: { type: String, default: null},
+  firstName: { type: String, required: true},
+  lastName: { type: String, required: true},
+  gender: { type: String, required: true, enum: Object.values(genderEnum)},
+
+});
+
